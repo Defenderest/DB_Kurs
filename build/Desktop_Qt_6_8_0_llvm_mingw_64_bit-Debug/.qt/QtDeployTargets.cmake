d31210b91c3e30b55f16a6f8e11a3c1faea2a1cb
Пацера Ігор <112869822+Defenderest@users.noreply.github.com>
2025-04-15 21:59:40 +0300

    Clean up repository and configure .gitignore

--- build/Desktop_Qt_6_8_0_llvm_mingw_64_bit-Debug/.qt/QtDeployTargets.cmake
+++
@@ -1,3 +0,0 @@
-set(__QT_DEPLOY_TARGET_untitled_FILE D:/projects/DB_Kurs/QtAPP/untitled/build/Desktop_Qt_6_8_0_llvm_mingw_64_bit-Debug/untitled.exe)
-set(__QT_DEPLOY_TARGET_untitled_TYPE EXECUTABLE)
-set(__QT_DEPLOY_TARGET_untitled_RUNTIME_DLLS D:/Qt/6.8.0/llvm-mingw_64/bin/Qt6Sql.dll;D:/Qt/6.8.0/llvm-mingw_64/bin/Qt6Widgets.dll;D:/Qt/6.8.0/llvm-mingw_64/bin/Qt6Gui.dll;D:/Qt/6.8.0/llvm-mingw_64/bin/Qt6Core.dll)
